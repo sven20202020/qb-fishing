b62eadbc8a30233afa136b0ad252450876d7965f
GhzGarage <gigahertzgarage@mail.com>
2021-06-17 00:21:18 -0500

    Initial Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-Fishing'
+version '1.0.0'
+
+shared_script 'config.lua'
+
+client_scripts {
+	'client/functions.lua',
+	'client/main.lua'
+}
+
+server_script 'server/main.lua'
